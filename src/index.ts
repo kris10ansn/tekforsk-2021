3c25b77d8d05078c0dd4ca705427dea67c88d8f7
kris10ansn <telefon.kristian@gmail.com>
2021-03-26 17:03:30 +0100

    remove redundant curly brackets

--- src/index.ts
+++ src/index.ts
@@ -15,8 +15,10 @@ const client = createClient(KEY!, ENDPOINT!);
 const createDataPath = dataPathFactory(createPath("data"));
 const imageNames = fs.readdirSync(createDataPath("/"));
 
-imageNames.map(createPipe(createDataPath, createStream)).forEach((image, i) => {
-    recognizeText(client, image)
-        .then(formatRecognitionResponse)
-        .then(logWithLabel(imageNames[i]));
-});
+imageNames
+    .map(createPipe(createDataPath, createStream))
+    .forEach((image, i) =>
+        recognizeText(client, image)
+            .then(formatRecognitionResponse)
+            .then(logWithLabel(imageNames[i]))
+    );
