63472824c2e0b899d26fdb0ef9c4d72db60dbe1c
kris10ansn <telefon.kristian@gmail.com>
2021-03-26 16:51:02 +0100

    add pipe and createPipe

--- src/util.ts
+++ src/util.ts
@@ -2,6 +2,16 @@ import { RecognizePrintedTextInStreamResponse } from "@azure/cognitiveservices-c
 import dotenv from "dotenv";
 import path from "path";
 
+export type Length<T extends any[]> = T extends { length: infer L } ? L : never;
+export type DropFirst<T extends any[]> = ((...args: T) => any) extends (
+    arg: any,
+    ...rest: infer U
+) => any
+    ? U
+    : T;
+
+export type LastElement<T extends any[]> = T[Length<DropFirst<T>>];
+
 export const logWithLabel = (label: string) => (...args: any) =>
     console.log(label, ...args);
 
@@ -21,3 +31,20 @@ export const env = () => {
 
 export const createPath = (...args: string[]) =>
     path.join(__dirname, "..", ...args);
+
+export const pipe = <
+    A,
+    F extends Array<(arg: A, ...args: any[]) => any>,
+    R extends ReturnType<LastElement<F>>
+>(
+    arg: A,
+    ...functions: F
+): R => functions.reduce((prev, cur) => cur(prev), arg) as R;
+
+export const createPipe = <
+    A,
+    F extends Array<(arg: A, ...args: any[]) => any>,
+    R extends ReturnType<LastElement<F>>
+>(
+    ...functions: F
+) => (arg: A) => pipe<A, F, R>(arg, ...functions);
