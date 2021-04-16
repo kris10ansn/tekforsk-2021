c08c0c86fdb43ba006ac81adb224162643e6ebdd
kris10ansn <telefon.kristian@gmail.com>
2021-04-16 14:47:03 +0200

    add recognizeCapture

--- src/index.ts
+++ src/index.ts
@@ -8,17 +8,33 @@ import {
     formatRecognitionResponse,
     logWithLabel,
 } from "./util";
+import { captureImage } from "./webcam";
 
 const { KEY, ENDPOINT } = env();
 const client = createClient(KEY!, ENDPOINT!);
-
 const createDataPath = dataPathFactory(createPath("data"));
-const imageNames = fs.readdirSync(createDataPath("/"));
 
-imageNames
-    .map(pipe(createDataPath, createStream))
-    .forEach((image, i) =>
+const recognizeAll = () => {
+    const imageNames = fs.readdirSync(createDataPath("/"));
+
+    imageNames
+        .map(pipe(createDataPath, createStream))
+        .forEach((image, i) =>
+            recognizeText(client, image)
+                .then(formatRecognitionResponse)
+                .then(logWithLabel(imageNames[i]))
+        );
+};
+
+const recognizeCapture = () => {
+    const imagePath = createDataPath("webcam.jpg");
+
+    captureImage(imagePath).then(() => {
+        const image = createStream(imagePath);
         recognizeText(client, image)
             .then(formatRecognitionResponse)
-            .then(logWithLabel(imageNames[i]))
-    );
+            .then(logWithLabel("webcam image"));
+    });
+};
+
+recognizeCapture();
