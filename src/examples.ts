daaebbcec787014b92706ac2f6a62b9eff0a3b89
kris10ansn <telefon.kristian@gmail.com>
2021-04-23 14:48:23 +0200

    move examples to example.ts

---
+++ src/examples.ts
@@ -0,0 +1,52 @@
+import { createClient, recognizeText } from "./azure";
+import { insertImage, destroyDatabaseConnection } from "./database";
+import { createStream, dataPathFactory } from "./files";
+import {
+    pipe,
+    recognitionResponseToArray,
+    logWithLabel,
+    createPath,
+    env,
+} from "./util";
+import { captureImage } from "./webcam";
+import fs from "fs";
+
+const { KEY, ENDPOINT } = env();
+const client = createClient(KEY!, ENDPOINT!);
+const createDataPath = dataPathFactory(createPath("data"));
+
+export const recognizeAll = () => {
+    const imageNames = fs.readdirSync(createDataPath("/"));
+
+    imageNames
+        .map(pipe(createDataPath, createStream))
+        .forEach((image, i) =>
+            recognizeText(client, image)
+                .then(recognitionResponseToArray)
+                .then(logWithLabel(imageNames[i]))
+        );
+};
+
+export const recognizeCapture = () => {
+    const imagePath = createDataPath("webcam.jpg");
+
+    captureImage(imagePath).then((image) => {
+        recognizeText(client, image)
+            .then(recognitionResponseToArray)
+            .then(logWithLabel("webcam image"));
+    });
+};
+
+export const recognizeAndInsert = async () => {
+    const imagePath = createDataPath("2.jpg");
+    const image = createStream(imagePath);
+
+    const result = await recognizeText(client, image);
+
+    const responseArray = recognitionResponseToArray(result);
+    const value = Number(responseArray[0][3][1]);
+
+    insertImage(imagePath, value)
+        .then(logWithLabel("insert"))
+        .finally(destroyDatabaseConnection);
+};
